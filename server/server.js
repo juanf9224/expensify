475dfce7c95b2c1876b4049e279cc1acc07a0f36
juanf9224 <juanfh.24.6@gmail.com>
2020-08-21 21:25:58 -0400

    setup prod build and server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
-import ReactDomServer from ' react-dom/server';
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
@@ -10,11 +10,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.get('/')
-app.get('https://jsonplaceholder.typicode.com/users', (req, res) => {
-
-});
-
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is running');
 });
