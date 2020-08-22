3a05732f6a66b27fd61a41bf1085359a438e269e
juanf9224 <juanfh.24.6@gmail.com>
2020-08-21 23:22:14 -0400

    add ExpenseListItem test and add numeraljs

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ import './styles/styles.scss';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 
 const store = configureStore();
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
 import { removeExpense } from '../actions/expenses';
-
+import numeral from 'numeraljs';
+import moment from 'moment';
 const ExpenseListItem = ({ id, description, amount, createdAt, dispatch }) => (
   
   <tr>
     <td>{description}</td>
-    <td>{amount}</td>
-    <td>{createdAt}</td>
+    <td>{numeral(amount / 100).format('$0,0.00')}</td>
+    <td>{moment(createdAt).format('MMMM Do, YYYY')}</td>
     <td><Link to={`/edit/${id}`}>Edit</Link></td>
     <td><button onClick={() => dispatch(removeExpense({ id }))}>Remove</button></td>
   </tr>
 );
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
